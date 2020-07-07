d803a60e1aba37070ba9e00e7252785a9b4e03f9
huangdejun1511425 <1968486175@qq.com>
2020-07-07 10:09:50 +0800

    7/7

--- appfront/src/components/consult/JobConsult.vue
+++ appfront/src/components/consult/JobConsult.vue
@@ -23,8 +23,8 @@
                 <Divider />
             </div>
             <div class="course">
-                <Tabs value="name1">
-                    <TabPane label="【快速预约】" name="name1">
+                <Tabs :value="displayTab">
+                    <TabPane label="【快速预约】" name="tab1">
                         <div v-for="item in course" :key=item.name class="course-list">
                             <div class="course-item">
                                 <div class="course-img"><img :src=item.url class="my-img-style"></div>
@@ -37,13 +37,14 @@
                                         </div>
                                     </div>
                                     <div class="check-time">
-                                        <Button type="primary" style="width:fit-content;float:left">查看可约时间</Button>
+                                        <Button type="primary" style="width:fit-content;float:left" @click="viewOrderTime(item)">查看可约时间</Button>
+                                        <Button type="primary" style="width:fit-content;margin-left:20px;float:left" v-if="item.isView" @click="onOrder(item)">{{item.date}}{{item.time}}</Button>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </TabPane>
-                    <TabPane label="【我的列表】" name="name2">
+                    <TabPane label="【我的列表】" name="tab2">
                         <div class="my-list">
                             <div class="list-title">
                                 <span>{{username}}</span>的预约列表
@@ -53,7 +54,7 @@
                                         <span style="padding-right:2%">{{item.date}}</span>{{item.time}}
                                     </div>
                                     <div class="consult-operate">
-                                        <a style="padding-right: 5%">更改预约</a><a>取消预约</a>
+                                        <a style="padding-right: 5%" @click="changeOrder(item)">更改预约</a><a>取消预约</a>
                                     </div>
                             </div>
                             <div v-for="item in orderCompleteList" :key=item.phone class="order-list">
@@ -85,9 +86,12 @@
                     预约信息
                 </div>
                 <div class="order-step-content">
-                    <Form :label-width="100" class="order-step-content-form">
-                        <FormItem label="上课平台" style="width: 100%;">
-                            <RadioGroup v-model="order_info.radio" style="margin-left:20px">
+                    <Form :label-width="120" class="order-step-content-form" :rules="ruleValidate" ref="order_info" :model="order_info">
+                        <FormItem label="课程id（可选）" style="width: 100%;">
+                            <Input v-model="order_info.courseId" placeholder="未填写课程id，将为您进行普通预约" style="margin-left:20px">
+                        </FormItem>
+                        <FormItem label="上课平台" style="width: 100%;" prop="platform">
+                            <RadioGroup v-model="order_info.platform" style="margin-left:20px">
                                 <!-- <Radio label="wechat"><img src="../../../static/img/wechat.jpg" class="order-step-way"></Radio>
                                 <Radio label="tencent"><img src="../../../static/img/tencent_meeting.jpg" class="order-step-way"></Radio>
                                 <Radio label="feishu"><img src="../../../static/img/fly_book.jpg" class="order-step-way"></Radio> -->
@@ -96,20 +100,20 @@
                                 <Radio label="feishu">腾讯会议</Radio>
                             </RadioGroup>
                         </FormItem>
-                        <FormItem label="所选平台账号" style="width: 100%;">
+                        <FormItem label="所选平台账号" style="width: 100%;" prop="account">
                             <Input v-model="order_info.account" placeholder="为了方便联系您，请填写所选平台账号..." style="margin-left:20px">
                         </FormItem>
-                        <FormItem label="选择时间" style="width: 100%;">
+                        <FormItem label="选择时间" style="width: 100%;" prop="date">
                             <DatePicker v-model="order_info.date" type="date" style="margin-left:20px"></DatePicker>
                         </FormItem>
-                        <FormItem label="预约时段" style="width: 100%;">
+                        <FormItem label="预约时段" style="width: 100%;" prop="time">
                             <RadioGroup v-model="order_info.time" style="margin-left:20px">
                                 <Radio label="上午">上午</Radio>
                                 <Radio label="下午">下午</Radio>
                             </RadioGroup>
                         </FormItem>
                         <FormItem style="width: 100%;">
-                            <Button type="primary" @click.native="submitOrder" style="margin-left:20px">确认预约</Button>
+                            <Button type="primary" @click.native="submitOrder" style="margin-left:20px">{{orderType}}</Button>
                             <Button style="margin-left: 20px" @click.native="cancelOrder">取消预约</Button>
                         </FormItem>
                     </Form>
@@ -119,6 +123,7 @@
     </div>
 </template>
 <script>
+import global_ from '../Const'
 import navigation from '../navigation'
 import { addCookie, getCookieValue, deleteCookie } from '../../cookie/useCookie'
 export default {
@@ -126,7 +131,11 @@ export default {
     data() {
         return {
             username: '',
-            isShow: false,
+            displayTab: "tab1",
+            isShow: true,   //咨询页，预约页切换
+            orderType: '确认预约',
+            oldOrderInfo: {},   //用于存储需要更改的预约列表
+            // isView: false,  //用户查看预约时间
             counselor: [
                 {
                     url: '../../../static/img/curry.jpg',
@@ -146,45 +155,60 @@ export default {
             ],
             course: [
                 {
+                    id: '1111',
                     url: '../../../static/img/curry.jpg',
                     title: '体验式免费咨询',
                     price: '0.00',
                     unit: '元/课时',
-                    pre_price: '99.99'
+                    pre_price: '99.99',
+                    date: '2020-07-20',
+                    time: '上午',
+                    isView: false
                 },
                 {
+                    id: '2222',
                     url: '../../../static/img/curry.jpg',
                     title: '体验式免费咨询',
                     price: '0.00',
                     unit: '元/课时',
-                    pre_price: '99.99'
+                    pre_price: '99.99',
+                    date: '2020-07-20',
+                    time: '上午',
+                    isView: false
                 },
                 {
+                    id: '1234',
                     url: '../../../static/img/curry.jpg',
                     title: '体验式免费咨询',
                     price: '0.00',
                     unit: '元/课时',
-                    pre_price: '99.99'
+                    pre_price: '99.99',
+                    date: '2020-07-20',
+                    time: '上午',
+                    isView: false
                 }
             ],
             orderList: [
                 {
-                    date: '2020-06-28',
-                    time: '上午',
-                    way: '飞书',
-                    phone: "12345678"
+                    courseID: '',
+                    platform: 'feishu',
+                    account: "12345679",
+                    date: '2020-06-29',
+                    time: '上午'
                 },
-                 {
-                    date: '2020-06-28',
-                    time: '上午',
-                    way: '飞书',
-                    phone: "12345678"
+                {
+                    courseID: '',
+                    platform: 'wechat',
+                    account: "12345678",
+                    date: '2020-06-30',
+                    time: '上午'
                 },
-                 {
-                    date: '2020-06-28',
-                    time: '上午',
-                    way: '飞书',
-                    phone: "12345678"
+                {
+                    courseID: '',     
+                    platform: 'tencent',
+                    account: "12345679",
+                    date: '2020-7-1',
+                    time: '上午'
                 }
             ],
             orderCompleteList: [
@@ -202,11 +226,27 @@ export default {
                 }
             ],
             order_info: {
-                radio: 'wechat',
+                courseId: '',
+                platform: '',
                 account: '',
-                date: "",
+                date: '',
                 time: '',
-            }
+            },
+            ruleValidate: {
+                    platform: [
+                        { required: true, message: '不能为空', trigger: 'change' }
+                    ],
+                    account: [
+                        { required: true, message: '不能为空', trigger: 'blur' }
+                    ],
+                    date: [
+                        { required: true, type: 'date', message: '不能为空', trigger: 'change' }
+                    ],
+                    time: [
+                        { required: true, message: '不能为空', trigger: 'change' }
+                    ]
+            },
+            infoValid: false,
         }
     },
     created() {
@@ -227,10 +267,86 @@ export default {
     },
     methods: {
         submitOrder() {
-
+            // console.log("写改后" + this.formTop.birthday)
+            // console.log(this.formTop)
+            if (this.orderType === '更改预约') {
+                for (var item in this.order_info) {
+                    this.oldOrderInfo[item] = this.order_info[item]
+                }
+            }
+            this.$refs['order_info'].validate((valid) => {
+                if (valid) {
+                    this.infoValid = true;
+                } else {
+                    this.$Message.error('填完必选项哦！');
+                }
+            })
+            if(this.infoValid == true) {
+                var d = new Date(this.order_info.date);
+                this.order_info.date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
+                console.log(this.order_info.date)
+                console.log(this.order_info)
+                // email = getCookieValue('email')
+                // let res = await fetchBase('/order_info', {
+                //         content: this.order_info
+                //     })
+                // // console.log(res)
+                // if (res['flag'] === global_.CONSTGET.SUCCESS) {
+                //     this.$Message.success("预约成功")
+                // } else if (res['flag'] === global_.CONSTGET.FAIL) {
+                //     this.$Message.error("预约失败")
+                // }
+                this.$Message.success("预约成功")
+                if (this.orderType === '更改预约') { 
+                    var isChange = false
+                    for (var item in this.order_info) {
+                        if (this.oldOrderInfo[item] != this.order_info[item]) {
+                            isChange = true
+                            break
+                        }
+                    }
+                    if (isChange) {
+                        for (var item in this.orderList) {
+                            if (JSON.stringify(this.oldOrderInfo) == JSON.stringify(this.orderList[item])) {
+                                this.orderList.splice(item, 1)
+                            }
+                        }
+                    }
+                }
+                var that = this
+                setTimeout(function () {
+                    that.orderList.push(that.order_info)
+                    that.isShow = true
+                }, 1000)
+                this.$refs['order_info'].resetFields()
+                console.log(this.order_info)
+            }
+            
         },
         cancelOrder() {
-            
+            this.$refs['order_info'].resetFields();
+        },
+        viewOrderTime(item) {
+            item.isView = true
+        },
+        onOrder(item) {
+            this.order_info.date = item.date
+            this.order_info.time = item.time
+            this.order_info.courseId = item.id
+            this.isShow = false
+            this.displayTab = 'tab2'
+        },
+        changeOrder(info) {
+            for (var item in info) {
+                this.order_info[item] = info[item]
+            }
+            this.isShow = false
+            this.orderType = '更改预约'
+            // console.log(this.orderList[0])
+            // console.log(this.orderList[1])
+            // console.log(this.orderList[2])
+            // console.log(JSON.stringify(this.orderList[0]) == JSON.stringify(this.orderList[1]))
+            // console.log(JSON.stringify(this.orderList[0]) == JSON.stringify(this.orderList[2]))
         }
     }
 }
@@ -278,6 +394,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    /* min-width: 300px; */
 }
 .img-style {
     width: 30%;
@@ -345,7 +462,7 @@ export default {
     height: fit-content;
     min-height: 400px;
     background-color: white;
-    border: 1px solid #755c5c;
+    /* border: 1px solid #755c5c; */
 }
 .list-title {
     width: 100%;
@@ -448,6 +565,10 @@ export default {
 }
 .order-step-content-form {
     width: 70%;
+    margin: 0 auto;
+    padding-bottom: 30px;
+}
+.order-consult >>> .ivu-form-item-error-tip {
     margin-left: 20px;
 }
 /* .order-step-content >>> .ivu-select-dropdown {
